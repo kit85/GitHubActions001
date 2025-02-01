6efcedb28f48140a78b4001036cb0373055c693b
kit85 <kitphung85@hotmail.com>
2025-02-01 16:38:00 +0100

    updated add with error.

--- src/test/java/calculator/MyClassTest.java
+++ src/test/java/calculator/MyClassTest.java
@@ -1,8 +1,7 @@
 package calculator;
 
-import org.junit.Test;
-
-import static junit.framework.TestCase.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class MyClassTest {
